f2ae5dad09c82598934fa93c333af6b59afac672
Jan Jaap Siewers <janjaap@siewers.org>
2025-02-24 16:10:48 +0100

    app en models en main netjes opgeruimd

--- app/models/token.py
+++ app/models/token.py
@@ -1,4 +1,25 @@
 from pydantic import BaseModel
+import os
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+import jwt
+from jwt.exceptions import InvalidTokenError
+from app.models.user import User, UserBase
+from app.database.conf import SessionDep, create_db_and_tables
+
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class Token(BaseModel):
     access_token: str
@@ -7,3 +28,35 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
+def get_current_user(session: SessionDep, token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user =  session.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+def authenticate_user(session: SessionDep, username: str, password: str):
+    user =  session.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not user.check_password(password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import os
 from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-import jwt
-from jwt.exceptions import InvalidTokenError
+from sqlmodel import Session, select
 from app.models.user import User, UserBase
-from app.models.token import Token, TokenData
+from app.models.token import Token, TokenData, get_current_user, authenticate_user, create_access_token
 from app.database.conf import SessionDep, create_db_and_tables
 
 from datetime import datetime, timedelta, timezone
@@ -16,30 +13,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# ALGORITHM = os.getenv("ALGORITHM")
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def get_current_user(session: SessionDep, token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-    user =  session.query(User).filter(User.username == username).first()
-    if user is None:
-        raise credentials_exception
-    return user
     
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+# @app.on_event("startup")
+# def on_startup():
+#     create_db_and_tables()
     
 @app.post("/token")
 async def login_for_access_token(
@@ -58,21 +42,6 @@ async def login_for_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
-
-
-# @app.get("/users/me/", response_model=User)
-# async def read_users_me(
-#     current_user: Annotated[User, Depends(get_current_active_user)],
-# ):
-#     return current_user
-
-
-
-# @app.get("/users/me/items/")
-# async def read_own_items(
-#     current_user: Annotated[User, Depends(get_current_active_user)],
-# ):
-#     return [{"item_id": "Foo", "owner": current_user.username}]
 
 @app.post("/users/")
 def create_user(user: User, session: SessionDep) -> User:
@@ -105,29 +74,6 @@ def read_user_by_username(username: str, session: SessionDep) -> UserBase:
 @app.get("/geheim")
 def geheim(token: str = Depends(get_current_user)):
     return "Dit is geheim!"
-    
-# def get_user_by_username(session: SessionDep, username: str):
-#     user =  session.query(User).filter(User.username == username).first()
-#     if user:
-#         return user
-#     else:
-#         return None
-    
-def authenticate_user(session: SessionDep, username: str, password: str):
-    user =  session.query(User).filter(User.username == username).first()
-    if not user:
-        return False
-    if not user.check_password(password):
-        return False
-    return user
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+
 
